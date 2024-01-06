c9b8f8fe9b8fb71b2a474b6a3521dd951845de22
MUHAMMAD ABDULLAH <135140627+mr-abd21@users.noreply.github.com>
2024-01-06 13:55:06 +0500

    fake user authentication for main page by creating protected route function

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/home/home";
 import Profile from "./pages/profile/profile";
 import {
   createBrowserRouter,
+  Navigate,
   Outlet,
   RouterProvider,
 } from "react-router-dom";
@@ -12,6 +13,10 @@ import Leftbar from "./components/leftbar/leftbar";
 import Rightbar from "./components/rightbar/rightbar";
 
 function App() {
+
+
+  //fake functionality to check if user loggrd in or not
+  const currentuser = false;
   //maintain layout of home page using react-router-dom outlet
   const Layout=()=>{
     return(
@@ -26,11 +31,20 @@ function App() {
     );
   };
 
+  //User Authentication for home page
+  const ProtectedRoute=({children})=>{
+    if(!currentuser){
+      return <Navigate to="/login"/>
+    }
+    return children
+  }
+
+
   //router dom sets the links between two pages
   const router = createBrowserRouter([
     {
       path:"/",
-      element: <Layout/>,
+      element: <ProtectedRoute><Layout/></ProtectedRoute>, //this protected route will check if there is a user then it will show the home page else redirect it to login pge
       children:[
         {
           path:"/",
