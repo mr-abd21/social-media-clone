f5f740af43dfa0d5d1e1cc24943a9ab07e51c9fe
MUHAMMAD ABDULLAH <135140627+mr-abd21@users.noreply.github.com>
2024-01-06 16:56:57 +0500

    Sticky and scroll to bars done to avoid overflow

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,33 @@
 import "./home.scss";
 
 const Home = () => {
-  return <div className="home">home</div>;
+  return (
+    <div className="home">
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+    </div>
+  );
 };
 export default Home;
