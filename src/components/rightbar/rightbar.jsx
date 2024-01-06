05b671a0bda87d9239737346db3ba88f2ebd1c81
MUHAMMAD ABDULLAH <135140627+mr-abd21@users.noreply.github.com>
2024-01-06 17:54:11 +0500

    Rightbar Frontend completed

--- src/components/rightbar/rightbar.jsx
+++ src/components/rightbar/rightbar.jsx
@@ -1,6 +1,135 @@
 import "./rightbar.scss";
 
 export const Rightbar = () => {
-  return <div className="rightbar">Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="container">
+        {/* first Item */}
+        <div className="item">
+          <span>Suggestions</span>
+          <div className="user">
+            <div className="userinfo">
+              <img
+                src="https://images.pexels.com/photos/1547971/pexels-photo-1547971.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <span>Mr abd</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Dismis</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userinfo">
+              <img
+                src="https://images.pexels.com/photos/1547971/pexels-photo-1547971.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <span>Mr abd</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Dismis</button>
+            </div>
+          </div>
+        </div>
+
+        {/* second Item */}
+
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userinfo">
+              <img
+                src="https://images.pexels.com/photos/1547971/pexels-photo-1547971.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <p>
+                <span>Mr abd</span> changed profile picture
+              </p>
+            </div>
+            <div className="buttons">
+              <span>1 min ago</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userinfo">
+              <img
+                src="https://images.pexels.com/photos/1547971/pexels-photo-1547971.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <p>
+                <span>Mr abd</span> changed profile picture
+              </p>
+            </div>
+            <div className="buttons">
+              <span>1 min ago</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userinfo">
+              <img
+                src="https://images.pexels.com/photos/1547971/pexels-photo-1547971.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <p>
+                <span>Mr abd</span> changed profile picture
+              </p>
+            </div>
+            <div className="buttons">
+              <span>1 min ago</span>
+            </div>
+          </div>
+        </div>
+
+        {/* third Item */}
+
+        <div className="item">
+          <span>Active Friends</span>
+          <div className="user">
+            <div className="userinfo">
+              <img
+                src="https://images.pexels.com/photos/1547971/pexels-photo-1547971.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Mr abd</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userinfo">
+              <img
+                src="https://images.pexels.com/photos/1547971/pexels-photo-1547971.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Mr abd</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userinfo">
+              <img
+                src="https://images.pexels.com/photos/1547971/pexels-photo-1547971.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Mr abd</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userinfo">
+              <img
+                src="https://images.pexels.com/photos/1547971/pexels-photo-1547971.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Mr abd</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Rightbar;
